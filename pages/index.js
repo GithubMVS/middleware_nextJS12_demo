304964a2a543e0df47c678cab961a07e6b51f69a
Matisse <matisse.van.steenbergen@student.howest.be>
2021-12-12 02:52:39 +0100

    some spelling checks + production bug fixed that you can't press on the go to redirect button??

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,12 @@ const index = () => {
     <div className={styles.middleware_demo_indexFull}>
       <div className={styles.goBack__full}>
         <Image src={goBack} alt='goBack' />
-        <Link href="https://nextjs.org"><a className={styles.links_learnMore}>See other demos</a></Link>
+        <Link href="https://passion-project-2021-2022.vercel.app/"><a className={styles.links_learnMore}>See other demos</a></Link>
       </div>
       <div className={styles.middleware_demo_explenation}>
         <h1 className={styles.middleware_demo_indexFull_title}>Middleware demo</h1>
-        <p className={styles.middleware_demo_indexFull_text}>This middleware demo will show you a glimps of what browser optemization could look like. But to put it in a more simple way: this demo will redirect you to a page that is optimized based on which browser you are so you can have the best performance at any time no matter what browser you are using all while you don't have to do a thing!</p>
-        <Link href="/redirect"><a className={styles.middleware_demo_indexFull_button}>go to specialised browser</a></Link>
+        <p className={styles.middleware_demo_indexFull_text}>This middleware demo will show you a glimps of what browser optemization could look like. But to put it in a more simple way: this demo will redirect you to a page that is optimized based on which browser you are so you can have the best performance at anytime no matter what browser you are using all while you don't have to do a thing!</p>
+        <Link href="https://middleware-next-js-12-demo.vercel.app/redirect"><a className={styles.middleware_demo_indexFull_button}>go to specialised browser</a></Link>
       </div>
     </div>
   )
