a9f2d7c6fc9208f714cc5dcb5372e942f06651eb
Matisse <matisse.van.steenbergen@student.howest.be>
2021-12-15 20:20:03 +0100

    testing out something in middleware demo 2

--- pages/demo2/demo.js
+++ pages/demo2/demo.js
@@ -1,9 +1,5 @@
 import React from 'react'
 
-export const getServerSideProps = ({ query }) => ({
-  props: query,
-})
-
 export default function demo({ country, latitude, longitude, city, region }) {
 
   country = decodeURIComponent(country)
@@ -13,12 +9,24 @@ export default function demo({ country, latitude, longitude, city, region }) {
   region = decodeURIComponent(region)
 
   return (
-    <div>
-      <p>{country}</p>
-      <p>{latitude}</p>
-      <p>{longitude}</p>
-      <p>{city}</p>
-      <p>{region}</p>
+    <div className={styles.demo}>
+      <p className={styles.demo_title}>This was all fetched before you had access to this page</p>
+      <div className={styles.demo_showed_full}>
+        <p className={styles.demo_showed_full_text}>You are currently in: {country}</p>
+        <p className={styles.demo_showed_full_text}>More specific in: {city}</p>
+        <p className={styles.demo_showed_full_text}>And your region is: {region}</p>
+        <p className={styles.demo_showed_full_text}>Your latitude is: {latitude}</p>
+        <p className={styles.demo_showed_full_text}>Your latitude is: {longitude}</p>
+      </div>
+
+      <ul className={styles.ideas_full}>
+        <p className={styles.ideas_full_title}>Cool projects you can do with this: </p>
+        <ul className={styles.ideas}>
+          <li className={styles.idea}></li>
+          <li className={styles.idea}></li>
+          <li className={styles.idea}></li>
+        </ul>
+      </ul>
     </div>
   )
 }
